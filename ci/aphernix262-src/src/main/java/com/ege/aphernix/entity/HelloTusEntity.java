b9e7ad4b42db86806c6dce0fce645ef9a3d024b9
Kadir Ergün <kadirergun@yandex.com>
2026-08-11 17:36:36 +0300

    Add Helllo Tus entity for Forge 26.2

---
+++ ci/aphernix262-src/src/main/java/com/ege/aphernix/entity/HelloTusEntity.java
@@ -0,0 +1,10 @@
+package com.ege.aphernix.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class HelloTusEntity extends AphernixEntity {
+    public HelloTusEntity(EntityType<? extends HelloTusEntity> type, Level level) {
+        super(type, level);
+    }
+}
