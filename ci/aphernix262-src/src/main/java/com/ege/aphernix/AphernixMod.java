c9419f70dc4fed76a51ec63c98b1da8af985e122
Kadir Ergün <kadirergun@yandex.com>
2026-08-11 17:34:37 +0300

    Port Aphernix entrypoint to full Forge 26.2 mod

--- ci/aphernix262-src/src/main/java/com/ege/aphernix/AphernixMod.java
+++ ci/aphernix262-src/src/main/java/com/ege/aphernix/AphernixMod.java
@@ -1,5 +1,6 @@
 package com.ege.aphernix;
 
+import com.ege.aphernix.registry.ModEntities;
 import com.ege.aphernix.registry.ModItems;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -9,7 +10,8 @@ public final class AphernixMod {
     public static final String MOD_ID = "aphernix";
 
     public AphernixMod(FMLJavaModLoadingContext context) {
-        // Forge 26.2 uses the mod BusGroup for deferred registrations.
-        ModItems.ITEMS.register(context.getModBusGroup());
+        var modBusGroup = context.getModBusGroup();
+        ModItems.ITEMS.register(modBusGroup);
+        ModEntities.ENTITY_TYPES.register(modBusGroup);
     }
 }
