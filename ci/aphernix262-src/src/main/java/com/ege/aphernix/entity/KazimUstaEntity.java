f369e695f272a046a5eff3d94b45e8c0b5873bff
Kadir Ergün <kadirergun@yandex.com>
2026-08-11 17:36:46 +0300

    Add Biyikli Kazim Usta entity for Forge 26.2

---
+++ ci/aphernix262-src/src/main/java/com/ege/aphernix/entity/KazimUstaEntity.java
@@ -0,0 +1,10 @@
+package com.ege.aphernix.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class KazimUstaEntity extends AphernixEntity {
+    public KazimUstaEntity(EntityType<? extends KazimUstaEntity> type, Level level) {
+        super(type, level);
+    }
+}
