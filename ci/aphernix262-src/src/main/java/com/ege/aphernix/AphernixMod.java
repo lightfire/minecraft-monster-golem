f9a5b8553d880b9a51352071a8eb792b309ad78f
Kadir Ergün <kadirergun@yandex.com>
2026-08-11 17:49:28 +0300

    Use EventBus 7 listeners for Aphernix 26.2

--- ci/aphernix262-src/src/main/java/com/ege/aphernix/AphernixMod.java
+++ ci/aphernix262-src/src/main/java/com/ege/aphernix/AphernixMod.java
@@ -1,7 +1,13 @@
 package com.ege.aphernix;
 
+import com.ege.aphernix.event.InteractionEvents;
+import com.ege.aphernix.event.ModEvents;
+import com.ege.aphernix.event.SchoolBattleEvents;
 import com.ege.aphernix.registry.ModEntities;
 import com.ege.aphernix.registry.ModItems;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -13,5 +19,10 @@ public final class AphernixMod {
         var modBusGroup = context.getModBusGroup();
         ModItems.ITEMS.register(modBusGroup);
         ModEntities.ENTITY_TYPES.register(modBusGroup);
+
+        EntityAttributeCreationEvent.BUS.addListener(ModEvents::createAttributes);
+        PlayerInteractEvent.EntityInteract.BUS.addListener(InteractionEvents::onEntityInteract);
+        PlayerInteractEvent.RightClickBlock.BUS.addListener(SchoolBattleEvents::onRightClickBlock);
+        LivingDeathEvent.BUS.addListener(SchoolBattleEvents::onDeath);
     }
 }
