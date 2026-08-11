61adaa60d98ec8cb6154eee7bbe2ef769f95b9d1
Kadir Ergün <kadirergun@yandex.com>
2026-08-11 17:50:32 +0300

    Port attribute registration to Forge EventBus 7

--- ci/aphernix262-src/src/main/java/com/ege/aphernix/event/ModEvents.java
+++ ci/aphernix262-src/src/main/java/com/ege/aphernix/event/ModEvents.java
@@ -1,18 +1,13 @@
 package com.ege.aphernix.event;
 
-import com.ege.aphernix.AphernixMod;
 import com.ege.aphernix.entity.AphernixEntity;
 import com.ege.aphernix.registry.ModEntities;
-import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.animal.chicken.Chicken;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = AphernixMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ModEvents {
     private ModEvents() {}
 
-    @SubscribeEvent
     public static void createAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.APHERNIX.get(), AphernixEntity.createAphernixAttributes().build());
         event.put(ModEntities.APHERNIX_TRANSFORMED.get(), AphernixEntity.createAphernixAttributes().build());
