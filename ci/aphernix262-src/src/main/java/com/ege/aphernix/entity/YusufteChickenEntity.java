c5592f3f620506fe4297495a793ad42284783131
Kadir Ergün <kadirergun@yandex.com>
2026-08-11 17:50:11 +0300

    Fix animal packages for Minecraft 26.2

--- ci/aphernix262-src/src/main/java/com/ege/aphernix/entity/YusufteChickenEntity.java
+++ ci/aphernix262-src/src/main/java/com/ege/aphernix/entity/YusufteChickenEntity.java
@@ -3,11 +3,11 @@ package com.ege.aphernix.entity;
 import com.ege.aphernix.registry.ModEntities;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.Chicken;
-import net.minecraft.world.entity.animal.Cow;
-import net.minecraft.world.entity.animal.Horse;
-import net.minecraft.world.entity.animal.Pig;
-import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.animal.chicken.Chicken;
+import net.minecraft.world.entity.animal.cow.Cow;
+import net.minecraft.world.entity.animal.equine.Horse;
+import net.minecraft.world.entity.animal.pig.Pig;
+import net.minecraft.world.entity.animal.sheep.Sheep;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 
